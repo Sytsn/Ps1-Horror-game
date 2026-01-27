567c1a1d27f319f30a11a5a60345d50b9b758980
Sytsn <jwclaus12@gmail.com>
2026-01-26 20:42:45 -0500

    replace project with my fps template project

---
+++ fps-template/Scripts/States/falling.gd
@@ -0,0 +1,19 @@
+extends PlayerState
+
+func enter(previous_state_path: String, data := {}) -> void:
+	pass
+func physics_update(delta: float) -> void:
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction := (player.neck.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		player.velocity.x = direction.x * player.speed
+		player.velocity.z = direction.z * player.speed 
+
+	player.velocity.y += player.gravity * delta
+	player.move_and_slide()
+
+	if player.is_on_floor():
+		if is_equal_approx(input_dir.x, 0.0) && is_equal_approx(input_dir.y, 0.0):
+			finished.emit(IDLE)
+		else:
+			finished.emit(RUNNING)
---
+++ fps-template/Scripts/player.gd
@@ -0,0 +1,44 @@
+class_name Player extends CharacterBody3D
+
+var mouse_sens := 0.001
+
+@export var speed := 10.0
+@export var gravity := -20.0
+@export var jump_impulse := 10.0
+
+@export var neck: Node3D
+@export var camera: Camera3D
+
+const SPEED = 10.0
+const JUMP_VELOCITY = 7
+
+@onready var is_paused = false
+
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_released("ui_cancel") and not is_paused:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		is_paused = true
+	elif Input.is_action_just_released("ui_cancel") and is_paused:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		is_paused = false
+
+
+func _physics_process(_delta: float) -> void:
+	pass
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		neck.rotate_y(-event.relative.x * mouse_sens)
+		camera.rotate_x(-event.relative.y * mouse_sens)
+		
+		camera.rotation.x = clamp(
+		camera.rotation.x,
+		deg_to_rad(-90),
+		deg_to_rad(90)
+	)
